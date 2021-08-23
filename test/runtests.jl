602b86484484f31ccf6381f90b5002975687ef0c
srinix <srinix@protonmail.com>
2021-08-23 22:58:57 +0530

    Tests added

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using ParallelArrays
 using Test
 
-@testset "ParallelArrays.jl" begin
-    # Write your tests here.
+@testset "Serial Collect" begin
+    M = rand(200, 100)
+    v = rand(100)
+    ML = LazyTensor(E)
+    vl = LazyTensor(v)
+
+    @test  ev(ML .* 3.0) ≈ M .* 3.0
+    @test  ev(ML .* vl) ≈ M .* v
+    @test ev(ML .* sin.(cos.(vl))) ≈ M .* sin.(cos.(v))
 end
