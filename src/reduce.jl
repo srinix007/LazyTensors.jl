035cb195c23864238492656b50f72f6a96fc8702
srinix <srinix@protonmail.com>
2021-08-25 02:52:39 +0530

    tsum working

--- src/reduce.jl
+++ src/reduce.jl
@@ -2,8 +2,8 @@
 tsum(A) = treduce(+, A)
 tsum(A,dims) = treduce(+, A, dims, zero(eltype(A)))
 
-prod(A) = treduce(*, A)
-prod(A, dims) = treduce(*, A, dims, one(eltype(A)))
+tprod(A) = treduce(*, A)
+tprod(A, dims) = treduce(*, A, dims, one(eltype(A)))
 
 treduce(op, A::AbstractArray) = threaded_reduce(op, A)
 
@@ -11,12 +11,14 @@ function treduce(op, A::AbstractArray{T}, dims, init::T) where {T}
     lred = prod(size(A)[i] for i in dims)
     lrem = length(A) - lred
 
-    if lred < minimum(1000, lrem)
-        threaded_reducedim_r(op, A, dims)
+    if lred < min(1000, lrem)
+        res = threaded_reducedim_r(op, A, dims)
     else
         dest = Base.reducedim_initarray(A, dims, init)
-        threaded_reducedim!(op, dest, A)
+        threaded_reducedim!(op, dest, A, dims, dest_dims(A, dims))
+        res = dest
     end
+    return dest
 end
 
 function halve(A::AbstractArray)
@@ -28,12 +30,10 @@ function halve(A::AbstractArray)
     return view(A, idx1), view(A, idx2)
 end
 
-function threaded_reduce(op, A, nth=Threads.nthreads)
+function threaded_reduce(op, A, nth=Threads.nthreads())
 
     if nth == 1
-        dest = zero(eltype(A))
-        Base.reduce(op, A)
-        return dest
+        return Base.reduce(op, A)
     end
 
     A1, A2 = halve(A)
@@ -46,13 +46,13 @@ end
 function halve(A::AbstractArray, rdims) 
     ldims = size(A)
     dims = length(ldims)
-    lrdims = map(x -> ldims[x], dims)
+    lrdims = map(x -> ldims[x], rdims)
     mid = lrdims .÷ 2
     j = 0
     k = 0
     midx = Tuple((i in rdims) ? (1:mid[j += 1]) : Colon() for i in 1:dims)
-    enx = Tuple((i in rdims) ? (mid[k += 1] + 1:ldims[k]) : Colon() for i in 1:dims)
-    return view(A, midx), view(A, enx)
+    enx = Tuple((i in rdims) ? ((1+mid[k += 1]):lrdims[k]) : Colon() for i in 1:dims)
+    return view(A, midx...), view(A, enx...)
 end
 
 
@@ -70,20 +70,21 @@ function threaded_reducedim_r(op, A, rdims, nth=Threads.nthreads())
     op.(threaded_reducedim_r(op, A2, rdims, nth3), fetch(t))
 end
 
-dest_dims(A, dims) = (i for i in 1:length(size(A)) if !(i in dims))
+dest_dims(A, dims) = Tuple(i for i in 1:length(size(A)) if !(i in dims))
 
-function threaded_reducedim!(op, dest, A, nth=Threads.nthreads())
+function threaded_reducedim!(op, dest, A, dims, rdims, nth=Threads.nthreads())
     
     if nth == 1
         Base.reducedim!(op, dest, A)
         return nothing
     end
 
-    D1, D2 = halve(D)
+    D1, D2 = halve(dest, rdims)
+    A1, A2 = halve(A, rdims)
     nth2 = nth >> 1
-    t = Threads.@spawn threaded_reducedim!(op, D1, A, nth2)
+    t = Threads.@spawn threaded_reducedim!(op, D1, A1, dims, rdims, nth2)
     nth3 = nth - nth2
-    threaded_reducedim!(op, D2, A, nth3)
+    threaded_reducedim!(op, D2, A2, dims, rdims, nth3)
     wait(t)
 
     return nothing
